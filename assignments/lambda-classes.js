9f97d6b02de197163aa79ff566e99d4d9fd3f0dc
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-01-24 16:51:51 -0500

    Wrote base-class and subclasses for lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,62 @@
 // CODE here for your Lambda Classes
+
+class Person {
+  constructor(atts) {
+    this.name = atts.name;
+    this.age = atts.age;
+    this.location = atts.location;
+    this.gender = atts.gender;
+  }
+  speak() {
+    console.log(`Hello my name is ${this.name}. I am from ${this.location}.`);
+  }
+}
+
+class Instructor extends Person {
+  constructor(atts) {
+    super(atts);
+    this.specialty = atts.specialty;
+    this.favLanguage = atts.favLanguage;
+    this.catchPhrase = atts.catchPhrase;
+  }
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}.`);
+  }
+  grade(student, subject) {
+    console.log(`${student.name} receives a perfect score on ${subject}.`);
+  }
+}
+
+class Student extends Person {
+  constructor(atts) {
+    super(atts);
+    this.previousBackground = atts.previousBackground;
+    this.className = atts.className;
+    this.favSubjects = atts.favSubjects;
+  }
+  listsSubjects() {
+    this.favSubjects.forEach(element => {
+      console.log(element);
+    });
+  }
+  PRAssignment(subject) {
+    console.log(`${this.name} has submitted a PR for ${subject}.`);
+  }
+  sprintChallenge(subject) {
+    console.log(`${this.name} has begun sprint challenge on ${subject}.`);
+  }
+}
+
+class ProjectManager extends Instructor {
+  constructor(atts) {
+    super(atts);
+    gradClassName = atts.gradClassName;
+    favInstructor = atts.favInstructor;
+  }
+  standup(channel) {
+    console.log(`${this.name} announces to ${channel}: @channel standy times!`);
+  }
+  debugsCode(student, subject) {
+    console.log(`${this.name} debugs ${student.name}'s code on ${subject}.`);
+  }
+}
